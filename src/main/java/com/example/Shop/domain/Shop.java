c6c39d116a91ad55dfa5143ef6433c95f64fa3c0
IgrayuVminecraftKakBozhenka <48388635+IgrayuVminecraftKakBozhenka@users.noreply.github.com>
2022-01-15 14:53:02 +0600

    Update Shop.java

--- src/main/java/com/example/Shop/domain/Shop.java
+++ src/main/java/com/example/Shop/domain/Shop.java
@@ -4,6 +4,7 @@ import com.example.Shop.entities.Person;
 import java.text.SimpleDateFormat;
 
 public class Shop {
+    
     public static double getPriceCoefficient(Person person) {
         if (new SimpleDateFormat("E").toString().equalsIgnoreCase("Воскресенье") && person.isRegular())
             return 0.8;
@@ -12,6 +13,7 @@ public class Shop {
         else if (new SimpleDateFormat("E").toString().equalsIgnoreCase("Воскресенье") && !person.isRegular())
             return 0.9;
         return 1;
+        
     }
 
     public static double getPriceCoefficient() {
